32c26b5591c79f7caedde492718b00a11b90519b
IGVDev <guidoverstraeten@gmail.com>
2024-06-07 15:48:36 -0300

    add league name and back button

--- src/pages/tournaments/view.tsx
+++ src/pages/tournaments/view.tsx
@@ -1,7 +1,8 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Heading, Spacer, Text } from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Link, useParams } from "wouter";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 export const TournamentView = () => {
   const params = useParams();
@@ -14,9 +15,21 @@ export const TournamentView = () => {
     return data;
   };
 
+  const getLeagueName = async (id: number) => {
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_API_URL}/tournaments/${id}`
+    );
+    return data.name;
+  };
+
   const { isPending, isError, data, error } = useQuery({
     queryKey: ["tournamentMatches", tournamentId],
     queryFn: () => getMatches(tournamentId),
+  });
+
+  const { data: leagueData } = useQuery({
+    queryKey: ["tournaments", tournamentId],
+    queryFn: () => getLeagueName(Number(tournamentId)),
   });
 
   if (isPending) {
@@ -28,81 +41,98 @@ export const TournamentView = () => {
   }
 
   return (
-    <Flex className="rounds">
-      {data.map((round, index: number) => (
+    <>
+      <Flex width="100%" alignItems="center" justifyContent="space-around">
+        <Flex justifySelf="start">
+          <Link to="/">
+            <IoMdArrowRoundBack size="35px" />
+          </Link>
+        </Flex>
+        <Flex justifySelf="center">
+          <Heading justifySelf="center">{leagueData}</Heading>
+        </Flex>
+        <Flex> </Flex>
+      </Flex>
+      <Flex className="rounds">
+        {data.map((round, index: number) => (
+          <Flex className="roundContainer">
+            <Flex className="round">
+              <Text>Round {index + 1}</Text>
+              {data[index].map((match) => (
+                <Link to={`/view/match/${match.id}`} className="match">
+                  <Flex className="match">
+                    <Flex
+                      className="team"
+                      justify="space-between"
+                      gap={8}
+                      fontWeight={
+                        match.winner === match.home_team_name
+                          ? "bold"
+                          : "normal"
+                      }
+                      bg={
+                        data[data.length - 1][0].winner === match.home_team_name
+                          ? "green.500"
+                          : "gray.400"
+                      }
+                      _dark={
+                        data[data.length - 1][0].winner === match.home_team_name
+                          ? { bg: "green.500" }
+                          : { bg: "gray.600" }
+                      }
+                    >
+                      <Text>{match.home_team_name}</Text>
+                      <Text>{match.home_result}</Text>
+                    </Flex>
+                    <Flex
+                      className="team"
+                      justify="space-between"
+                      gap={8}
+                      fontWeight={
+                        match.winner === match.away_team_name
+                          ? "bold"
+                          : "normal"
+                      }
+                      bg={
+                        data[data.length - 1][0].winner === match.away_team_name
+                          ? "green.500"
+                          : "gray.500"
+                      }
+                      _dark={
+                        data[data.length - 1][0].winner === match.away_team_name
+                          ? { bg: "green.500" }
+                          : { bg: "gray.700" }
+                      }
+                    >
+                      <Text>{match.away_team_name}</Text>
+                      <Text>{match.away_result}</Text>
+                    </Flex>
+                  </Flex>
+                </Link>
+              ))}
+            </Flex>
+          </Flex>
+        ))}
         <Flex className="roundContainer">
           <Flex className="round">
-            <Text>Round {index + 1}</Text>
-            {data[index].map((match) => (
-              <Link to={`/view/match/${match.id}`} className="match">
-                <Flex className="match">
-                  <Flex
-                    className="team"
-                    justify="space-between"
-                    gap={8}
-                    fontWeight={
-                      match.winner === match.home_team_name ? "bold" : "normal"
-                    }
-                    bg={
-                      data[data.length - 1][0].winner === match.home_team_name
-                        ? "green.500"
-                        : "gray.400"
-                    }
-                    _dark={
-                      data[data.length - 1][0].winner === match.home_team_name
-                        ? { bg: "green.500" }
-                        : { bg: "gray.600" }
-                    }
-                  >
-                    <Text>{match.home_team_name}</Text>
-                    <Text>{match.home_result}</Text>
-                  </Flex>
-                  <Flex
-                    className="team"
-                    justify="space-between"
-                    gap={8}
-                    fontWeight={
-                      match.winner === match.away_team_name ? "bold" : "normal"
-                    }
-                    bg={
-                      data[data.length - 1][0].winner === match.away_team_name
-                        ? "green.500"
-                        : "gray.500"
-                    }
-                    _dark={
-                      data[data.length - 1][0].winner === match.away_team_name
-                        ? { bg: "green.500" }
-                        : { bg: "gray.700" }
-                    }
-                  >
-                    <Text>{match.away_team_name}</Text>
-                    <Text>{match.away_result}</Text>
-                  </Flex>
-                </Flex>
-              </Link>
-            ))}
-          </Flex>
-        </Flex>
-      ))}
-      <Flex className="roundContainer">
-        <Flex className="round">
-          <Text>Champion</Text>
-          <Flex className="match">
-            <Flex
-              className="team"
-              bg={data[data.length - 1][0].winner ? "green.500" : "gray.400"}
-              fontWeight={data[data.length - 1][0].winner ? "bold" : "normal"}
-              _dark={
-                data[data.length - 1][0].winner
-                  ? { bg: "green.500" }
-                  : { bg: "gray.400" }
-              }
-            >
-              {data[data.length - 1][0].winner || "TBD"}
+            <Text>Champion</Text>
+            <Flex className="match">
+              <Flex
+                className="team"
+                bg={data[data.length - 1][0].winner ? "green.500" : "gray.400"}
+                fontWeight={data[data.length - 1][0].winner ? "bold" : "normal"}
+                _dark={
+                  data[data.length - 1][0].winner
+                    ? { bg: "green.500" }
+                    : { bg: "gray.400" }
+                }
+              >
+                {data[data.length - 1][0].winner || "TBD"}
+              </Flex>
             </Flex>
           </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 };
